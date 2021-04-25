61be46ce7f1db9f8d83b200f8d582690c704849f
EC2 Default User <ec2-user@ip-172-31-22-169.us-west-1.compute.internal>
2021-04-25 20:01:57 +0000

    updated files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -3,20 +3,13 @@
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
-dockerpath=udacityproject4
+dockerpath="sarasalem2410/udacityproject4:latest"
 
 # Step 2
-# Run the Docker Hub container with kubernetes
-kubectl run $dockerpath --image=sarasalem2410/udacityproject4:v1pro3 --port=80
+kubectl run udacityproject4 --image=$dockerpath --port=80
 
 # Step 3:
-# List kubernetes pods
-kubectl get pod
+kubectl get pods
+
 # Step 4:
-# Forward the container port to a host
-kubectl expose deployment udacityproject4 --type=LoadBalancer --port=80
-minikube service udacityproject4
-
-
+kubectl port-forward udacityproject4 8000:80
